a948d7524372a4b86a4eea6e09e8fe7658fa09d8
monosniper <monosniper@gmail.com>
2024-03-14 23:18:53 +0500

    asdas

--- src/components/LastBuys.js
+++ src/components/LastBuys.js
@@ -15,24 +15,22 @@ const LastBuys = () => {
             <div className="last-buy__container">
                 <h2 className="last-buy__title title">Последние покупки:</h2>
                 <div className="last-buy__items">
-                    {store.last_buys.map(buy => {
-                        buy.Products.map(product => (
-                            <div className="buy-item" data-popup="#info">
-                                <div className="buy-item__label">
-                                    {buy.created_at}
-                                </div>
-                                <div className="buy-item__img -ibg">
-                                    <img src={API_URL + product.image} alt={product.title}/>
-                                </div>
-                                <div className="buy-item__title">
-                                    <span>{product.title}</span>
-                                    <small className="_icon-user">
-                                        {buy.name}
-                                    </small>
-                                </div>
+                    {store.last_buys.map(buy => buy.Products.map(product => (
+                        <div className="buy-item" data-popup="#info">
+                            <div className="buy-item__label">
+                                {buy.created_at}
                             </div>
-                        ))
-                    })}
+                            <div className="buy-item__img -ibg">
+                                <img src={API_URL + product.image} alt={product.title}/>
+                            </div>
+                            <div className="buy-item__title">
+                                <span>{product.title}</span>
+                                <small className="_icon-user">
+                                    {buy.name}
+                                </small>
+                            </div>
+                        </div>
+                    )))}
                 </div>
             </div>
         </section>
