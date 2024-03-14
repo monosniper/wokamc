342c69c967e06a02ee1165a2d01d40d7582353d0
monosniper <monosniper@gmail.com>
2024-03-14 06:07:03 +0500

    dasd

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,44 +7,42 @@ import ProductChoice from "./modals/ProductChoice";
 
 const Product = ({product, className}) => {
     const {store} = useContext(Context);
-    console.log(store.products.find(({id}) => id === product.id))
-    return store.products.find(({id}) => id === product.id).hide ? null : (
-        <div className={"product-wrapper " + className}>
-            <div className={"product"}>
-                <div className={"product__container"} onClick={() => store.showModal('productInfo', product.id)}>
-                    <div className="product__title">{product.title}</div>
-                    <div className="product__img -ibg">
-                        <img src={API_URL + product.image} alt={product.title}/>
-                    </div>
-                    {product.discount ? <div className="product__discount">{product.discount}%</div> : ''}
-                </div>
-                <div className="product__footer">
-                    <div className="product__price">{product.price_1}.00 ₽</div>
-                    {/*{store.basket.find(item => item.id === product.id) ? (*/}
-                    {/*    <div className="product__action">*/}
-                    {/*        <button className="btn-buy btn-buy_detail" type="button">*/}
-                    {/*            {store.basket.find(item => item.id === product.id).count} шт.*/}
-                    {/*        </button>*/}
-                    {/*        <button onClick={() => store.removeFromBasket(product.id)} className="btn-del _icon-del"*/}
-                    {/*                type="button"></button>*/}
-                    {/*    </div>*/}
-                    {/*) : (*/}
-                    {/*    <button onClick={() => store.addToBasket(product.id)}*/}
-                    {/*            className="btn-buy btn-buy_common _icon-cart" type="button">*/}
-                    {/*        В корзину*/}
-                    {/*    </button>*/}
-                    {/*)}*/}
-                    <button onClick={() => store.showModal('productChoice', product.id)}
-                            className="btn-buy btn-buy_common _icon-cart" type="button">
-                        В корзину
-                    </button>
-                </div>
-            </div>
 
-            <ProductInfo product={product} tags={store.tags}/>
-            <ProductChoice product={product}/>
+    return <div className={"product-wrapper " + className}>
+        <div className={"product"}>
+            <div className={"product__container"} onClick={() => store.showModal('productInfo', product.id)}>
+                <div className="product__title">{product.title}</div>
+                <div className="product__img -ibg">
+                    <img src={API_URL + product.image} alt={product.title}/>
+                </div>
+                {product.discount ? <div className="product__discount">{product.discount}%</div> : ''}
+            </div>
+            <div className="product__footer">
+                <div className="product__price">{product.price_1}.00 ₽</div>
+                {/*{store.basket.find(item => item.id === product.id) ? (*/}
+                {/*    <div className="product__action">*/}
+                {/*        <button className="btn-buy btn-buy_detail" type="button">*/}
+                {/*            {store.basket.find(item => item.id === product.id).count} шт.*/}
+                {/*        </button>*/}
+                {/*        <button onClick={() => store.removeFromBasket(product.id)} className="btn-del _icon-del"*/}
+                {/*                type="button"></button>*/}
+                {/*    </div>*/}
+                {/*) : (*/}
+                {/*    <button onClick={() => store.addToBasket(product.id)}*/}
+                {/*            className="btn-buy btn-buy_common _icon-cart" type="button">*/}
+                {/*        В корзину*/}
+                {/*    </button>*/}
+                {/*)}*/}
+                <button onClick={() => store.showModal('productChoice', product.id)}
+                        className="btn-buy btn-buy_common _icon-cart" type="button">
+                    В корзину
+                </button>
+            </div>
         </div>
-    );
+
+        <ProductInfo product={product} tags={store.tags}/>
+        <ProductChoice product={product}/>
+    </div>;
 };
 
 export default observer(Product);
