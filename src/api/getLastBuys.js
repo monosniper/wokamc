68eca0298d46f64f883d798b9aa00410fb50ce01
monosniper <monosniper@gmail.com>
2024-03-24 02:05:19 +0500

    dasd

--- src/api/getLastBuys.js
+++ src/api/getLastBuys.js
@@ -1,4 +1,4 @@
 import {$api} from "./index";
 
 export const getLastBuys = async () =>
-    (await $api.get('buys?limit=10&sort=["createdAt","DESC"]')).data
+    (await $api.get('buys?limit=10&sort=["createdAt","DESC"]&isCompleted=true')).data
